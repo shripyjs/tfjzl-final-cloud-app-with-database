28347e348f9ec49e1d367cd479bd538211ad52d0
shripyjs <shripadborbande2005@gmail.com>
2026-03-27 11:32:47 -0400

    Added submit and exam result views and urls

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -20,5 +20,7 @@ urlpatterns = [
     # <HINT> Create a route for submit view
 
     # <HINT> Create a route for show_exam_result view
+    path('<int:course_id>/submit/', views.submit, name="submit"),
+    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name="exam_result"),
 
  ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
